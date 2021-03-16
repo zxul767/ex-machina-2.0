3ca2309d6f56e899c4ac486ce99e74b3c5ac6797
zxul767 <willebaldo.gomez@gmail.com>
2021-03-15 21:25:56 -0600

    Update website configuration details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Ex-Machina`,
+    name: `Ex-Machina`,
+    siteUrl: `https://zxul767.dev`,
+    description: `Programming, data science, machine learning and cryptocurrencies!`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to Ex-Machina.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/zxul767`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://github.com/zxul767`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/in/wgomezj`,
       },
     ],
   },
@@ -48,8 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
+        name: `Ex-Machina`,
+        short_name: `Ex-Machina`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#fff`,
